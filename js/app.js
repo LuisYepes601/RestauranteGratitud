60518e83d8312dc34350c9d216b5f0d513c665e3
Luis <yepesluis006@gmail.com>
2025-05-23 16:18:03 -0500

    primer_commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+const header_nav = document.getElementById("header_nav_lista");
+const btn_menu = document.getElementById("btn_menu_header");
+
+btn_menu.addEventListener("click", ()=>{
+    header_nav.classList.add("header_nav_lista_active")
+})
+
+
